2f332801680470bb947a8ff9abc9713e64755bf1
Michael <mirima92@gmail.com>
2024-11-24 00:53:26 -0500

    Set up auto-shooting mechanics

--- gun.gd
+++ gun.gd
@@ -5,3 +5,16 @@ func _physics_process(delta: float) -> void:
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+
+
+func shoot():
+	const BULLET = preload("res://bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	%ShootingPoint.add_child(new_bullet)
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	
+
+
+func _on_timer_timeout() -> void:
+	shoot()
